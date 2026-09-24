117336fd816ce8337aca46a097bac44b867cd281
Una <una.lestrange@atu.ie>
2026-09-24 12:49:12 +0100

    task 3 done

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, {Application, Request, Response} from "express" ;
 import carRoutes from './routes/cars'
 import {env} from './config/env';
 import { connectDB } from "./config/database";
+import { authenticateKey } from "./middleware/auth.middleware";
 
 const port = env.port
 
@@ -9,10 +10,16 @@ const app: Application = express();
 
 app.use(express.json());
 
+// Middleware to log incoming requests
+
 app.use((req, _res, next) => {
     console.log(`${req.method} ${req.originalUrl}`);
     next();
 });
+
+// Middleware to authenticate API key
+
+//app.use(authenticateKey);
 
 app.get("/ping", async (_req : Request, res: Response) => {
     res.json({
@@ -26,7 +33,7 @@ app.get('/bananas', async (_req : Request, res: Response) => {
     });
 });
 
-app.use('/api/v1/cars', carRoutes);
+app.use('/api/v1/cars', authenticateKey, carRoutes);
 
 
 const startServer = async () => {
